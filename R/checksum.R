578b0ab6102cafaed09d923cedb1886e6130289e
alanboth <alan.both@rmit.edu.au>
2021-02-11 16:56:31 +1100

    adding expectedDistances.rds and vistaSummaries to checksum

--- R/checksum.R
+++ R/checksum.R
@@ -7,7 +7,13 @@ checksum <- function() {
     '../data/distanceMatrixIndex.csv' = 'ae0bc43e02033bc2df150e665dd43134', 
     '../data/SA1attributed.csv.gz' = '980faffc7c90996510123ce4d69f140e', 
     '../data/SA1centroids.csv.gz' = 'b7f27ff1e176b70cee71723f528f800a', 
-    '../data/addresses.csv.gz' = 'ed761018dfdf4e00fdf2054aa25fe3ba'
+    '../data/addresses.csv.gz' = 'ed761018dfdf4e00fdf2054aa25fe3ba',
+    '../data/expectedDistances.rds' = '91e137c02459e21a2fabf541e8cc36de',
+    '../data/vistaSummaries/destinationProbabilitiesSA3.rds' = '7dbacc829b312d608b6f24df7235b028',
+    '../data/vistaSummaries/distanceDistributionsSA3.rds' = '9ded1a901e720c3f670b5ecda6c105f8',
+    '../data/vistaSummaries/distanceHistograms.rds' = '7b4a6f836c56e53be5a9409a4886d42a',
+    '../data/vistaSummaries/modeChoiceProbabilitiesSA3.rds' = 'ef6c190af7e1ee7f5c474811a5bf0dee',
+    '../data/vistaSummaries/vistaDistanceHistograms.rds' = '2243e27a7ac46e64f2c0f2a68751606b'
   )
   actual<-md5sum(names(expected))
   
