27b07b2e8b1c06b342823577fa327d3c8aeab89d
jafshin <af.jafari.1992@gmail.com>
2021-09-10 12:17:41 +1000

    Updating the checksum for the new addresses.csv

--- R/checksum.R
+++ R/checksum.R
@@ -3,12 +3,12 @@ library(tools) # for md5sum
 checksum <- function() {
   expected<- list(
     '../data/VISTA_12_18_CSV.zip.dir/T_VISTA1218_V1.csv' = 'f16040c1a12c96df8ba1b18d9e6cabf0',
-    '../data/absRegionsReprojected.sqlite' = '43a51bb3dbd5cfdabad23645d7a0dc87', 
-    '../data/distanceMatrix.rds' = 'cafb401785b656f3be660f387a2c3be2', 
-    '../data/distanceMatrixIndex.csv' = 'ae0bc43e02033bc2df150e665dd43134', 
-    '../data/SA1attributed.csv.gz' = '64d2031ff63d5496def5ff06784b8697', 
-    '../data/SA1centroids.csv.gz' = 'b7f27ff1e176b70cee71723f528f800a', 
-    '../data/addresses.csv.gz' = 'f12d95af85eecacf56e7c9b7839c2221',
+    '../data/absRegionsReprojected.sqlite' = '43a51bb3dbd5cfdabad23645d7a0dc87',
+    '../data/distanceMatrix.rds' = 'cafb401785b656f3be660f387a2c3be2',
+    '../data/distanceMatrixIndex.csv' = 'ae0bc43e02033bc2df150e665dd43134',
+    '../data/SA1attributed.csv.gz' = '64d2031ff63d5496def5ff06784b8697',
+    '../data/SA1centroids.csv.gz' = 'b7f27ff1e176b70cee71723f528f800a',
+    '../data/addresses.csv.gz' = '43bca5fa2d814ce3337c8a01da6841e3',
     '../data/vistaCohorts.csv.gz' = 'd9f0c9ad76fc31513afbc9348b747e32',
     '../data/expectedDistances.rds' = '0d725dcfc2603a94399b78523fecc078',
     '../data/vistaSummaries/destinationProbabilitiesSA3.rds' = '938309b079c9bdeae46254d9c6dae321',
@@ -17,7 +17,7 @@ checksum <- function() {
     '../data/vistaSummaries/modeChoiceProbabilitiesSA3.rds' = 'c25809d0f3fd0653320a06c50eff4a82'
   )
   actual<-md5sum(names(expected))
-  
+
   result <- TRUE
   for (file in names(expected)) {
     e <- expected[file]
